969185e121aadf13bcd4485d938125ef68c09c7a
Shreeraj-Deshmukh <shreeraj.deshmukh.22031@iitgoa.ac.in>
2026-05-05 16:46:38 +0530

    .next file updated for deployment
    stale variables changed

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -19,6 +19,7 @@ export async function POST(req: Request) {
       data: {
         name: body.name,
         email: body.email,
+        role: body.role ?? 'BD',
       },
     })
 
--- prisma.config.ts
+++ prisma.config.ts
@@ -5,10 +5,6 @@ export default defineConfig({
   schema: "./prisma/schema.prisma",
   migrations: {
     path: "./prisma/migrations",
-    seed: "tsx prisma/seed.ts",   // 👈 ADD THIS
+    seed: "tsx prisma/seed.ts",
   },
-datasource: {
-  url: process.env.DATABASE_URL,
-  directUrl: process.env.DIRECT_URL,   // add this
-},
 });
