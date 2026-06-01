4861ada8567e99cf8b93d233a29109d6a2b93981
Shreeraj-Deshmukh <shreeraj.deshmukh.22031@iitgoa.ac.in>
2026-06-01 20:57:19 +0530

    procdnanexa debug2

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,8 @@
 import { withAuth } from 'next-auth/middleware'
 
-export const proxy = withAuth
+export const proxy = withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+})
 
 export const config = {
   matcher: ['/((?!login|login2|api/auth|api/approvals/email-action|api/emergency|_next/static|_next/image|favicon.ico).*)'],
